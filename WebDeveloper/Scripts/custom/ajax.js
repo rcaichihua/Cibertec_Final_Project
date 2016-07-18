381d96b4d0146891c06d0b3a4a664cbdd6406025
juvega <cevegaju@hotmail.com>
2016-07-17 22:44:44 -0500

    Test modal popup

---
+++ WebDeveloper/Scripts/custom/ajax.js
@@ -0,0 +1,23 @@
+﻿//(function () {
+//    var promise
+//})();
+
+function getResource(address) {
+    var promise = $.ajax({
+        url: address,
+        statusCode: {
+            404: function () {
+                alert("page not found");
+            }
+        }
+    });
+    promise.done(function(response)
+    {
+        var dialog = $("#divDialog");
+        if (dialog)
+        {
+            dialog.html(response).dialog();            
+        }        
+    });
+
+}
