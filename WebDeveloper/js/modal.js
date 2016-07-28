d6872d15804767cd39d015152d8f55bf589491fd
rcaichihua <rcaichihua@gmail.com>
2016-07-27 20:39:08 -0500

    cambios

--- WebDeveloper/js/modal.js
+++ WebDeveloper/js/modal.js
@@ -1,4 +1,8 @@
-﻿function getModal(url) {
+﻿$(document).ready(function () {
+    $(".datepicker").datepicker();
+})
+
+function getModal(url) {
     $.get(url, function (data) {
         $('.modal-content').html(data);
         $('#modal-container').modal('show');
